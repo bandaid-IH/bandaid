6b9dd626116fcec9874abe4446b618c16c1488d2
Jad ch <jad.chalita@gmail.com>
2017-10-09 15:51:40 +0200

    signup and login done, next: bandcamp setup

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
-
-const index = require('./routes/index');
+const passport = require('passport')
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy
+const mongoose = require('mongoose')
 
 const app = express();
+
+// Data Models
+const User = require('./models/user')
+
+// Controllers
+const index = require('./routes/index')
+
+// Mongoose Configuration
+mongoose.connect("mongodb://localhost/bandaid")
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +37,59 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// APP CONFIG
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+// PASSSPORT CONFIG
+passport.serializeUser((user, cb) => {
+  cb(null, user._id)
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({
+    "_id": id
+  }, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, {
+        message: "Incorrect username"
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {
+        message: "Incorrect password"
+      });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// Initializing Session
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+const User = require('../models/user')
+
+const router = express.Router()
+
+// ====== Home Page ======
+router.get('/', (req, res, next) => {
+  res.render('index')
 });
 
-module.exports = router;
+// ====== Sign-Up Page ======
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup', {
+    errorMessage: false
+  })
+})
+
+router.post('/signup', (req, res, next) => {
+  let salt = bcrypt.genSaltSync(bcryptSalt)
+  let hashPass = bcrypt.hashSync(req.body.password, salt)
+
+  User.findOne({
+    username: req.body.username
+  }, (error, user) => {
+    if (user) res.render('auth/signup', {
+      errorMessage: 'This username is already taken :('
+    })
+    else {
+      let newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        bandcampUsername: '',
+        bandcampID: '',
+        email: '',
+        favoriteArtists: []
+      })
+
+      newUser.save((error) => {
+        if (error) res.render('auth/signup', {
+          errorMessage: 'Something went wrong'
+        })
+        else {
+          res.redirect('/bandcampsetup')
+        }
+      })
+    }
+  })
+})
+
+// ====== Log-In Page ======
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', {
+    errorMessage: false
+  })
+})
+
+router.post('/login', passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: false,
+      passReqToCallback: true
+    }));
+
+    // ====== Bandcamp Set-Up Page ======
+    router.get('/bandcampsetup', (req, res, next) => {
+      res.render('auth/bandcampsetup')
+    })
+
+
+    module.exports = router;
