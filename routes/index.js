ef7627b7bedb4b60e1d6c85c4081df2069e78470
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-10-10 09:23:28 +0200

    Added test query to bandcamp feed

--- routes/index.js
+++ routes/index.js
@@ -1,113 +1,156 @@
-const express = require('express')
-const passport = require('passport')
-const bcrypt = require('bcrypt')
-const axios = require('axios')
-const qs = require('qs')
-const bcryptSalt = 10
-const connectEnsure = require('connect-ensure-login')
-const ensureLoggedIn = connectEnsure.ensureLoggedIn('/login');
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const axios = require("axios");
+const qs = require("qs");
+const bcryptSalt = 10;
+const connectEnsure = require("connect-ensure-login");
+const ensureLoggedIn = connectEnsure.ensureLoggedIn("/login");
 
-const User = require('../models/user')
+const User = require("../models/user");
 
-const router = express.Router()
+const router = express.Router();
 
 // ====== Home Page ======
-router.get('/', (req, res, next) => {
-  res.render('index')
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 // ====== Sign-Up Page ======
-router.get('/signup', (req, res, next) => {
-  res.render('auth/signup', {
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup", {
     errorMessage: false
-  })
-})
+  });
+});
 
-router.post('/signup', (req, res, next) => {
-  let salt = bcrypt.genSaltSync(bcryptSalt)
-  let hashPass = bcrypt.hashSync(req.body.password, salt)
+router.post("/signup", (req, res, next) => {
+  let salt = bcrypt.genSaltSync(bcryptSalt);
+  let hashPass = bcrypt.hashSync(req.body.password, salt);
 
-  User.findOne({
-    username: req.body.username
-  }, (error, user) => {
-    if (user) res.render('auth/signup', {
-      errorMessage: 'This username is already taken :('
-    })
-    else {
-      let newUser = new User({
-        username: req.body.username,
-        password: hashPass,
-        bandcampUsername: '',
-        bandcampID: '',
-        email: '',
-        favoriteArtists: []
-      })
+  User.findOne(
+    {
+      username: req.body.username
+    },
+    (error, user) => {
+      if (user)
+        res.render("auth/signup", {
+          errorMessage: "This username is already taken :("
+        });
+      else {
+        let newUser = new User({
+          username: req.body.username,
+          password: hashPass,
+          bandcampUsername: "",
+          bandcampID: "",
+          email: "",
+          favoriteArtists: []
+        });
 
-      newUser.save((error) => {
-        if (error) res.render('auth/signup', {
-          errorMessage: 'Something went wrong'
-        })
-        else {
-          res.redirect('/bandcampsetup')
-        }
-      })
+        newUser.save(error => {
+          if (error)
+            res.render("auth/signup", {
+              errorMessage: "Something went wrong"
+            });
+          else {
+            res.redirect("/bandcampsetup");
+          }
+        });
+      }
     }
-  })
-})
+  );
+});
 
 // ====== Log-In Page ======
-router.get('/login', (req, res, next) => {
-  res.render('auth/login', {
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", {
     errorMessage: false
-  })
-})
+  });
+});
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/home',
-  failureRedirect: '/login',
-  failureFlash: false,
-  passReqToCallback: true
-}));
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+    failureFlash: false,
+    passReqToCallback: true
+  })
+);
 
 // ====== Bandcamp Set-Up Page ======
-router.get('/bandcampsetup', ensureLoggedIn, (req, res, next) => {
-  res.render('auth/bandcampsetup', {errorMessage: false})
-})
+router.get("/bandcampsetup", ensureLoggedIn, (req, res, next) => {
+  res.render("auth/bandcampsetup", { errorMessage: false });
+});
 
-router.post('/bandcampsetup', ensureLoggedIn, (req, res, next) => {
-  let BCusername = req.body.bandcampUsername
+router.post("/bandcampsetup", ensureLoggedIn, (req, res, next) => {
+  let BCusername = req.body.bandcampUsername;
   getBandcampID(BCusername)
-  .then( (id) => {
-    User.findByIdAndUpdate({_id: req.user._id}, { bandcampID: id }, (error) => {
-      res.redirect('/home')
+    .then(id => {
+      User.findByIdAndUpdate(
+        { _id: req.user._id },
+        { bandcampID: id },
+        error => {
+          res.redirect("/home");
+        }
+      );
     })
-  })
-  .catch( (error) => {
-    res.render('auth/bandcampsetup', {errorMessage: "Couldn't find this username on Bandcamp"})
-  })
-})
+    .catch(error => {
+      res.render("auth/bandcampsetup", {
+        errorMessage: "Couldn't find this username on Bandcamp"
+      });
+    });
+});
 
-function getBandcampID (BCusername) {
-  return axios.get(`https://bandcamp.com/${BCusername}`)
-  .then( (response) => {
-    let response_str = response['data']
-    let start_index = response_str.indexOf('id="follow-unfollow') + 20
-    let ID = response_str.substring(start_index, response_str.indexOf('"', start_index))
-    return ID
-  })
-  .catch( (error) => {
-    throw error
-  })
+function getBandcampID(BCusername) {
+  return axios
+    .get(`https://bandcamp.com/${BCusername}`)
+    .then(response => {
+      let response_str = response["data"];
+      let start_index = response_str.indexOf('id="follow-unfollow') + 20;
+      let ID = response_str.substring(
+        start_index,
+        response_str.indexOf('"', start_index)
+      );
+      return ID;
+    })
+    .catch(error => {
+      throw error;
+    });
 }
 
 // ====== Bandaid User Home ======
-router.get('/home', ensureLoggedIn, (req, res, next) => {
-  res.render('home')
-})
+router.get("/home", ensureLoggedIn, (req, res, next) => {
+  res.render("home");
+});
 
 // ====== Fetching User Bandcamp feed ======
 
+// Following is Eduardo's code with fan_id replaced
 
+async function main() {
+  try {
+    const a = await axios.post(
+      "https://bandcamp.com/fan_dash_feed_updates",
+      qs.stringify({
+        // fan_id: 2079572,
+        fan_id: 2079572,
+        // older_than: 1483605566
+      }),
+      {
+        headers: {
+          Cookie:
+            "client_id=066071D749E770DABEDA6A53D3D59BAEE19651A0DCE19E0A8A667F165E24FDBF; unique_24h=223; want_https=1; identity=6%09efbc233f4349282ad61de8f592ab1fda%09%7B%22id%22%3A4147611151%2C%22h1%22%3A%22819f670ef1890f499a5c62baa315ffb8%22%2C%22ex%22%3A0%7D; session=1%09t%3A1507291939%09r%3A%5B%22261569836S0c0x1507292079%22%2C%22241753119x0c0x1507292003%22%2C%2218185G0f0x1507291939%22%5D; fan_visits=503214; BACKENDID=bender06-1",
+          "Content-type": "application/x-www-form-urlencoded"
+        }
+      }
+    );
 
+    console.log(a.data.stories.entries);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
 
 module.exports = router;
